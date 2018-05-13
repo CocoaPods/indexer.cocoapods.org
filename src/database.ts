cc909b55410f35ed5d555e39786b6bdca998318b
Julian Gutierrez Ferrara <juliangutierrezferrara@gmail.com>
2018-05-13 14:54:46 -0400

    Initialize with ssl the right way

--- src/database.ts
+++ src/database.ts
@@ -1,6 +1,8 @@
 import { Client } from 'pg';
 
-export const trunk = new Client(process.env.TRUNK_DATABASE_URL);
-trunk.ssl = true;
+export const trunk = new Client({
+    connectionString: process.env.TRUNK_DATABASE_URL,
+    ssl: true
+});
 
 export const setup = async () => await trunk.connect();
