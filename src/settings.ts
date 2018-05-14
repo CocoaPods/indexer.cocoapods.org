146edca3c616c65c865c5b20fd7388bcb7375908
Haroen Viaene <hello@haroen.me>
2018-05-14 15:55:38 +0200

    feat(settings):  searchable authors

--- src/settings.ts
+++ src/settings.ts
@@ -2,7 +2,12 @@ import { IndexSettings, Synonym, Rule } from 'algoliasearch';
 
 export const settings: IndexSettings = {
   searchableAttributes: ['name', 'summary', 'authors.name'],
-  attributesForFaceting: ['platforms', 'license.type', 'requires_arc'],
+  attributesForFaceting: [
+    'platforms',
+    'license.type',
+    'requires_arc',
+    'searchable(authors.name)',
+  ],
   // @ts-ignore
   camelCaseAttributes: ['name'],
 };
