b85fce122b3d99910d55c230457a4af499fcce43
Haroen Viaene <hello@haroen.me>
2018-05-14 15:48:36 +0200

    feat(settings): add camel casing

--- src/settings.ts
+++ src/settings.ts
@@ -3,6 +3,8 @@ import { IndexSettings, Synonym, Rule } from 'algoliasearch';
 export const settings: IndexSettings = {
   searchableAttributes: ['name', 'summary', 'authors.name'],
   attributesForFaceting: ['platforms', 'license.type', 'requires_arc'],
+  // @ts-ignore
+  camelCaseAttributes: ['name'],
 };
 
 export const synonyms: Synonym[] = [];
