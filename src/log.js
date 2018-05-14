ba806b54cbfd2ede4bf3c88a7e07907cb24d2ac9
Haroen Viaene <hello@haroen.me>
2018-05-14 10:33:03 +0200

    chore(logging): allow passing of log level via env

--- src/log.js
+++ src/log.js
@@ -3,5 +3,6 @@ require('dotenv').config();
 
 const log = bunyan.createLogger({
   name: process.env.ALGOLIA_INDEX_NAME || 'unknown',
+  level: process.env.LOG_LEVEL || 'INFO',
 });
 module.exports = log;
