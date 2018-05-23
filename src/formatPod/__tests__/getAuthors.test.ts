8423b6ec1d7661933239231872685df39cb44c05
Haroen Viaene <hello@haroen.me>
2018-05-23 16:43:25 +0200

    feat(format): allow more forms of authors

--- src/formatPod/__tests__/getAuthors.test.ts
+++ src/formatPod/__tests__/getAuthors.test.ts
@@ -1,4 +1,57 @@
-import { getAuthor, getAvatar } from '../getAuthors';
+import { getAuthor, getAvatar, getAuthors } from '../getAuthors';
+import { SpecificationData } from '../../types';
+
+describe('getAuthors', () => {
+  it('works with arrays of strings', () => {
+    const authors = ['apple', 'google'];
+
+    expect(getAuthors({ authors } as SpecificationData)).toEqual([
+      { name: 'apple' },
+      { name: 'google' },
+    ]);
+  });
+
+  it('works with arrays of objects and strings', () => {
+    const authors = [
+      'Dustin Voss',
+      {
+        'Robbie Hanson': 'robbiehanson@deusty.com',
+      },
+    ];
+
+    expect(getAuthors({ authors } as SpecificationData)).toEqual([
+      {
+        name: 'Dustin Voss',
+      },
+      {
+        name: 'Robbie Hanson',
+        email: 'robbiehanson@deusty.com',
+        avatar: expect.any(String),
+      },
+    ]);
+  });
+
+  it('works with objects', () => {
+    const authors = {
+      'Dustin Voss': 'dustins@email.com',
+      'Robbie Hanson': 'robbiehanson@deusty.com',
+    };
+
+    // @ts-ignore not actually sure how to type this correctly ({name: email}, {name, email},string, mixed[])
+    expect(getAuthors({ authors } as SpecificationData)).toEqual([
+      {
+        name: 'Dustin Voss',
+        email: 'dustins@email.com',
+        avatar: expect.any(String),
+      },
+      {
+        name: 'Robbie Hanson',
+        email: 'robbiehanson@deusty.com',
+        avatar: expect.any(String),
+      },
+    ]);
+  });
+});
 
 describe('getAuthor', () => {
   it('flips keys around', () => {
@@ -6,7 +59,7 @@ describe('getAuthor', () => {
       Haroen: 'hello@haroen.me',
     };
 
-    expect(getAuthor(author)).toEqual({
+    expect(getAuthor(author)[0]).toEqual({
       avatar: expect.any(String),
       email: 'hello@haroen.me',
       name: 'Haroen',
@@ -14,7 +67,7 @@ describe('getAuthor', () => {
   });
 
   it('ignores undefined', () => {
-    expect(getAuthor(undefined)).toBe(undefined);
+    expect(getAuthor(undefined)).toEqual([]);
   });
 });
 
--- src/formatPod/getAuthors.ts
+++ src/formatPod/getAuthors.ts
@@ -1,23 +1,40 @@
 import gravatarUrl from 'gravatar-url';
 import { SpecificationData } from '../types';
 
+const flatten = <T>(singlyNestedArray: T[][]) =>
+  Array.prototype.concat(...singlyNestedArray);
+
+interface Author {
+  name: string;
+  email?: string;
+  avatar?: string;
+}
+
 export function getAuthors({ authors }: SpecificationData) {
   if (typeof authors === 'string') {
     const [name, email] = authors.split(' => '); // can also be a ruby hash (should be fixed in the DB)
     return [{ name, email, ...getAvatar(email) }];
   }
   if (Array.isArray(authors)) {
-    return authors.map(author => getAuthor(author));
+    const auth = flatten<Author>(authors.map(author => getAuthor(author)));
+    return auth;
   }
-  return [getAuthor(authors)];
+  return [...getAuthor(authors)];
 }
 
-export function getAuthor(author?: { [name: string]: string }) {
+export function getAuthor(author?: { [name: string]: string } | string) {
+  if (typeof author === 'string') {
+    return [
+      {
+        name: author,
+      },
+    ];
+  }
   return Object.entries(author || {}).map(([name, email]) => ({
     name,
     email,
     ...getAvatar(email),
-  }))[0];
+  }));
 }
 
 export function getAvatar(email: string = '') {
