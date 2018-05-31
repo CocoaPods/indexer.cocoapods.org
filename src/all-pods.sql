832f149cb143352cfdcd51ff5a46d2aba096d550
Haroen Viaene <hello@haroen.me>
2018-05-31 15:58:26 +0200

    fix(db): fetch latest published version
    before the max(name) was only working within a same major version.
    
    This is also a simpler way of writing the query.
    
    Thanks @kmatrah
    
    cc @jugutier

--- src/all-pods.sql
+++ src/all-pods.sql
@@ -1,63 +1,24 @@
--- Reference: https://gist.github.com/jugutier/62d1da6fc8edc3d6efe88223b33f5032
+WITH latest_version AS (
+  SELECT id, pod_id FROM (
+    SELECT
+      id,
+      pod_id,
+      rank() OVER (PARTITION by pod_id ORDER BY created_at desc) AS version_rank
+      FROM pod_versions
+    ) AS ranked_versions
+  WHERE version_rank = 1
+)
+
 SELECT
   pods.normalized_name AS "objectID",
-  specification_data AS "specificationData",
+  commits.specification_data AS "specificationData",
   json_build_object(
     'lastMonth', stats_metrics.download_month,
     'total', stats_metrics.download_total,
-    'appsTouched', stats_metrics.app_total) AS downloads
+    'appsTouched', stats_metrics.app_total
+  ) AS downloads
 
-FROM
-  pods,
-  stats_metrics,
-  -- get the last version of a pod
-  (
-    SELECT
-      latest_version_per_pod.pod_id,
-      all_pods.specification_data
-
-    FROM (
-        SELECT
-          latest_version,
-          pod_versions.id AS version_id,
-          latest_pods.pod_id
-
-        FROM pod_versions
-
-        JOIN (
-            SELECT
-              max(pod_versions.name) AS latest_version,
-              pod_id
-
-            FROM pod_versions
-
-            GROUP BY pod_id
-          ) AS latest_pods ON pod_versions.pod_id = latest_pods.pod_id
-          AND pod_versions.name = latest_pods.latest_version
-      ) AS latest_version_per_pod
-
-      LEFT JOIN LATERAL
-
-      (
-        SELECT
-          pod_versions.name AS VERSION,
-          commits.specification_data,
-          pod_versions.pod_id,
-          pod_versions.id AS version_id
-
-        FROM commits
-
-        JOIN pod_versions ON commits.pod_version_id = pod_versions.id
-
-        WHERE
-          pod_versions.pod_id = latest_version_per_pod.pod_id
-          AND version_id = latest_version_per_pod.version_id
-
-        LIMIT 1
-      ) AS all_pods ON TRUE
-  ) as latest_version
-
-WHERE
-  pods.id = latest_version.pod_id
-  AND pods.id = stats_metrics.pod_id
-  AND NOT pods.deleted
+FROM pods
+LEFT JOIN latest_version ON latest_version.pod_id = pods.id
+LEFT JOIN stats_metrics ON stats_metrics.pod_id = pods.id
+LEFT JOIN commits ON commits.pod_version_id = latest_version.id;
