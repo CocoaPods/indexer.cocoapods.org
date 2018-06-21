7f47521b6c362e38114e3cd15b72a0bbc447d868
Haroen Viaene <hello@haroen.me>
2018-06-21 11:56:46 +0200

    chore: move query inline
    for some reason changes weren't detected in the separate file, so will leave it inline like before

--- src/database.ts
+++ src/database.ts
@@ -2,7 +2,33 @@ import { Client } from 'pg';
 import { Pod, SpecificationData } from './types';
 import log from './log';
 import { formatPod } from './formatPod/index';
-import allPodsQuery from './all-pods.sql';
+
+const allPodsQuery = `
+WITH latest_version AS (
+  SELECT id, pod_id FROM (
+    SELECT
+      id,
+      pod_id,
+      rank() OVER (PARTITION by pod_id ORDER BY created_at desc) AS version_rank
+      FROM pod_versions
+    ) AS ranked_versions
+  WHERE version_rank = 1
+)
+
+SELECT
+  pods.normalized_name AS "objectID",
+  commits.specification_data AS "specificationData",
+  json_build_object(
+    'lastMonth', stats_metrics.download_month,
+    'total', stats_metrics.download_total,
+    'appsTouched', stats_metrics.app_total
+  ) AS downloads
+
+FROM pods
+LEFT JOIN latest_version ON latest_version.pod_id = pods.id
+LEFT JOIN stats_metrics ON stats_metrics.pod_id = pods.id
+LEFT JOIN commits ON commits.pod_version_id = latest_version.id
+`;
 
 export const trunk = new Client({
   connectionString: process.env.TRUNK_DATABASE_URL,
